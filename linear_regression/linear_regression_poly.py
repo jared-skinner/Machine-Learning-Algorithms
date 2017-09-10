a1bdb9a7410cd061f4fd22d06e8131b4d2b6641a
Jared Skinner <jaredds66@gmail.com>
2017-09-10 16:04:22 -0500

    updated polynomial linear regression to used vectorized form of things.  More effecent, more flexible and easier to understand

--- linear_regression/linear_regression_poly.py
+++ linear_regression/linear_regression_poly.py
@@ -6,44 +6,51 @@ X = np.sort(np.random.rand(30)) * 10
 noise = (np.sort(np.random.rand(30)) - .5) * 30
 y = np.multiply(- .002 * np.power(X, 6) + .3 * np.power(X, 5) + .002 * np.power(X, 4) + .00000000000033 * X + 10, noise)
 
+# we know from the dummy data that a 6th degree polynomial should model this
+# well.  With that in mind, we will create a feature for each power of X: 0
+# through 7.
+X_features = []
+for i in range(0, 7):
+    x_pow = np.power(X, i)
+    X_features.append(x_pow)
+
+# here is our new features vector!
+X_features = np.array(X_features)
+
+# plot X and y to get an idea what we are looking at.
 plt.plot(X, y, 'ro')
 plt.show()
 
+# learning rate and number of epochs.  TODO: figure out why the learning rate
+# has to be so low to prevent divergence
 learning_rate = .0000000000001
 number_of_epochs = 100000
 
-# weights for function f = W[0] + W[1]x + W[2]x^2 + W[3]x^3 + W[4]x^4
-W = np.random.rand(7)
-
-def approx_function(x_vals, weights):
-    y_approx = weights[0] + weights[1] * x_vals + weights[2] * np.power(x_vals, 2) + weights[3] * np.power(x_vals, 3) + weights[4] * np.power(x_vals, 4) + weights[6] * np.power(x_vals, 6) + weights[6] * np.power(x_vals, 6)
-    return y_approx
-    
+# training weights
+W = (np.random.rand(7) - .5) * 10
 
 for epoch in range(number_of_epochs):
     # create predictions
-    y_approx = approx_function(X, W)
-    
+    y_approx = np.matmul(W, X_features)
+
     # calculate cost
     cost = np.sum(1/2 * np.power(y - y_approx,2))
         
     # calculate gradient
-    d_cost_d_w0 = np.sum(-(y - y_approx))
-    d_cost_d_w1 = np.sum(-(y - y_approx) * X)
-    d_cost_d_w2 = np.sum(-(y - y_approx) * np.power(X, 2))
-    d_cost_d_w3 = np.sum(-(y - y_approx) * np.power(X, 3))
-    d_cost_d_w4 = np.sum(-(y - y_approx) * np.power(X, 4))
-    d_cost_d_w5 = np.sum(-(y - y_approx) * np.power(X, 6))
-    d_cost_d_w6 = np.sum(-(y - y_approx) * np.power(X, 6))
-        
-    grad = np.array([d_cost_d_w0, d_cost_d_w1, d_cost_d_w2, d_cost_d_w3, d_cost_d_w4, d_cost_d_w5, d_cost_d_w6])
-    
+    grad = np.sum(-(y - y_approx) * X_features, axis=1)
+
     # move weights
     W = W - learning_rate * grad
     
     if (epoch + 1) % 6000 == 0 or epoch == 0:
         X_plot_vals = np.arange(0, 10, .001)
-        y_plot_vals = approx_function(X_plot_vals, W)
+
+        X_plot_powers = []
+        for i in range(0,7):
+            x_pow = np.power(X_plot_vals, i)
+            X_plot_powers.append(x_pow)
+
+        y_plot_vals = np.matmul(W, X_plot_powers)
         plt.plot(X, y, 'ro')
         plt.plot(X_plot_vals, y_plot_vals)
         plt.show()
