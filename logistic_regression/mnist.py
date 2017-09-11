50834fc727a9c7d245954bf80a299b4c2a9d4e8c
Jared Skinner <jaredds66@gmail.com>
2017-09-10 22:26:52 -0500

    started filling in mnist example for logistic regression

---
+++ logistic_regression/mnist.py
@@ -0,0 +1,22 @@
+from logistic_regression import LogisticRegression
+
+
+# import data
+
+# massage data
+
+# get model
+logistic = LogisticRegression()
+
+
+# shuffle and split
+
+# train!
+
+# cross validate
+
+# test
+
+# pickle for later use
+
+
