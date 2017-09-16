d068deb8a02da2050e87467fbe6a0c7ddac3d3f4
Jared Skinner <jaredds66@gmail.com>
2017-09-16 16:02:19 -0500

    started work on mnist data set

--- neural_network_test.py
+++ neural_network_test.py
@@ -3,6 +3,14 @@ import numpy as np
 from neural_network import NeuralNetwork
 from training_model import TrainingModel
 
+import scipy.misc
+import matplotlib.pyplot as plt
+import os
+
+
+
+
+
 
 # TODO: fill these out and learn how to do TDD!!!
 class TestHappy(unittest.TestCase):
@@ -18,8 +26,42 @@ class TestHappy(unittest.TestCase):
 
 
 def main():
+
+    image_paths = os.listdir("mnist/Images/train/")
+
+    images = []
+    for image_path in image_paths:
+        print(image_path)
+
+        # read image from training data
+        image = scipy.misc.imread(os.path.join("mnist/Images/train/", image_path))
+
+        # read in corresponding value from csv file
+
+        
+        #plt.imshow(image)
+        #plt.show()
+
+        image = np.ndarray.flatten(image)
+
+        images.append(image)
+
+        if len(images) > 1000:
+            break
+
+
+    images = np.array(images)
+    print(images.shape)
+
+    # todo: pickle these for latter
+
+
+    exit()
+
+
+
     # dummy example
-    X = np.array([[1,1], [1,3], [4,4]]).reshape(3,2)
+    X = np.array([[2,1], [1,3], [4,4]]).reshape(3,2)
     y = np.array([0, 10, 10]).reshape(3,1)
     learning_rate = .03
     layers = np.array([2, 3, 1])
@@ -28,10 +70,10 @@ def main():
 
     nn = NeuralNetwork(layers=layers, X=X, y=y, learning_rate=learning_rate, weight_decay=weight_decay, activation_fn=TrainingModel.rect_lin, number_of_epochs=number_of_epochs, plot_cost_graph=True)
 
-    test = np.array([[4, 4]]).reshape(1,2)
-    _,_,approx = nn.foward_feed(test)
-
-    print(approx)
+    #test = np.array([[4, 4]]).reshape(1,2)
+    #_,_,approx = nn.foward_feed(test)
+#
+#    print(approx)
 
 
     nn.train_model()
