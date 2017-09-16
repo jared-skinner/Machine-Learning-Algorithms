cffa7ba3b0a52512d197cbecc4942ff13628260d
Jared Skinner <jaredds66@gmail.com>
2017-09-15 23:41:48 -0500

    some more tinkering

--- neural_network.py
+++ neural_network.py
@@ -1,22 +1,27 @@
 import numpy as np
-import matplotlib as pyplot
+import matplotlib.pyplot as plt
 from training_model import TrainingModel
 
 
-# TODO: add in bias terms
 class NeuralNetwork(TrainingModel):
     '''
     basic neural network
     '''
 
-    def __init__(self, layers, X, y, learning_rate = 1, weight_decay = 0):
+    def __init__(self, layers, X, y, learning_rate = 1, weight_decay = 0, activation_fn=TrainingModel.sigmoid, number_of_epochs=10, plot_cost_graph=False):
+
+
+        self.plot_cost_graph = plot_cost_graph
 
         # layers should be a list (or np array) of sizes, where each index
         # represents a layer from left to right, and the number is the number of
         # nodes
         self.layers = layers
-        self.number_of_hidden_layers = len(self.layers) - 2
+        self.number_of_epochs = number_of_epochs
+        # the rate at which we want to perform gradient descent
+        self.learning_rate = learning_rate
 
+        # X and y should have the same number of examples!
         assert X.shape[0] == y.shape[0]
 
         # regularization parameter.  real number
@@ -41,9 +46,15 @@ class NeuralNetwork(TrainingModel):
             # bias value for everything except the output layer
             self.biases.append(np.random.rand(next_layer).reshape(1, next_layer))
 
+        # gradient
         self.grad = []
 
-        self.learning_rate = learning_rate
+        # map of functions and their deratives
+        derivative_dict = {self.tanh: self.tanh_prime, self.sigmoid: self.sigmoid_prime, self.rect_lin: self.rect_lin_prime}
+
+        self.activation_fn = activation_fn
+        self.activation_fn_prime = derivative_dict[activation_fn]
+
 
 
         #super(NeuralNetwork, self).__init__()
@@ -68,7 +79,7 @@ class NeuralNetwork(TrainingModel):
         for weight, bias in zip(self.weights, self.biases):
             z = np.matmul(a, weight) + bias
             layer_activations.append(z)
-            a = self.tanh(z)
+            a = self.activation_fn(z)
             layer_output.append(a)
 
         y_approx = a
@@ -76,8 +87,6 @@ class NeuralNetwork(TrainingModel):
         return layer_activations, layer_output, y_approx
 
 
-
-    # TODO: add in regulization term
     def cost(self):
         '''
         compute to cost
@@ -95,6 +104,7 @@ class NeuralNetwork(TrainingModel):
         return cost
 
 
+    # TODO: support batch back prop
     def back_prop(self, x, y):
         '''
         perform back propigation to updated the weights
@@ -112,7 +122,7 @@ class NeuralNetwork(TrainingModel):
         z = layer_activations[-1]
 
         # calculate the gradient of the output layer
-        delta.insert(0, -(y - a) * self.tanh_prime(z))
+        delta.insert(0, -(y - a) * self.activation_fn_prime(z))
 
         a = layer_output[len(layer_output)-2]
 
@@ -139,36 +149,60 @@ class NeuralNetwork(TrainingModel):
             if len(delta) == len(self.weights):
                 break
 
-            delta.insert(0, np.matmul(weight, delta[0]) * self.tanh_prime(z).T)
+            delta.insert(0, np.matmul(weight, delta[0]) * self.activation_fn_prime(z).T)
+
+
+    def train_model(self):
+        no_exs = self.X.shape[0]
+
+        for epoch in range(self.number_of_epochs):
+
+
+            for i in range(self.X.shape[0]):
+                self.back_prop(self.X[[i],:], self.y[[i],:])
+
+                for i, _ in enumerate(self.weights):
+
+                    self.weights[i] = self.weights[i] - self.learning_rate * (1/no_exs * self.grad[i] + self.weight_decay * self.weights[i])
+                    self.biases[i] = self.biases[i] - 1/no_exs * self.learning_rate * self.bias_grad[i]
+
+            if self.plot_cost_graph:
+                if epoch % np.round(self.number_of_epochs/100) == 0:
+                    plt.plot(epoch, self.cost(), 'ro')
+
+            if epoch % np.round(self.number_of_epochs/10) == 0:
+                print(self.cost())
+
+
+        if self.plot_cost_graph:
+            plt.show()
+
+
+
 
 def main():
     # dummy example.  Will eventually move to neural_network_test.py
     #nn = NeuralNetwork(layers=np.array([15, 10, 1]), X=np.array([[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15], [1,2.3,3.5,4,5,6,7,8,9,19,11,12,13,14,15], [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]]).reshape(3, 15), y=np.array([-1,1,1]).reshape(3,1), learning_rate=.001)
-    X = np.array([[1,1], [3,3], [4,4]]).reshape(3,2)
-    y = np.array([-1, 1, 1]).reshape(3,1)
-    learning_rate = .8
-    layers = np.array([2, 10, 10, 10, 1])
-    weight_decay = .001
+    X = np.array([[1,1], [1,3], [4,4]]).reshape(3,2)
+    y = np.array([0, 10, 10]).reshape(3,1)
+    learning_rate = .03
+    layers = np.array([2, 3, 1])
+    weight_decay = .0#001
+    number_of_epochs = 500
 
-    nn = NeuralNetwork(layers=layers, X=X, y=y, learning_rate=learning_rate, weight_decay = weight_decay)
+    nn = NeuralNetwork(layers=layers, X=X, y=y, learning_rate=learning_rate, weight_decay=weight_decay, activation_fn=TrainingModel.rect_lin, number_of_epochs=number_of_epochs, plot_cost_graph=True)
 
-    number_of_epochs = 4000
-
-    no_exs = X.shape[0]
-
-    for epoch in range(number_of_epochs):
-        print(nn.cost())
-
-        for i in range(nn.X.shape[0]):
-            nn.back_prop(nn.X[[i],:], nn.y[[i],:])
-
-            for i, _ in enumerate(nn.weights):
-
-                nn.weights[i] = nn.weights[i] - nn.learning_rate * (1/no_exs * nn.grad[i] + weight_decay * nn.weights[i])
-                nn.biases[i] = nn.biases[i] - 1/no_exs * nn.learning_rate * nn.bias_grad[i]
-
-    test = np.array([[-1, -1]]).reshape(1,2)
+    test = np.array([[4, 4]]).reshape(1,2)
     _,_,approx = nn.foward_feed(test)
+
+    print(approx)
+
+
+    nn.train_model()
+
+    test = np.array([[4, 4]]).reshape(1,2)
+    _,_,approx = nn.foward_feed(test)
+
     print(approx)
 
 
